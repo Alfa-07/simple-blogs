ddbd4dd024a06556c78e8c060e63006d16ba3cd3
Ansari Arif <aa5736301@gmail.com>
2024-11-03 23:34:41 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import styles from '../styles/Footer.module.css';
 
 export default function Footer() {
   return (
-    <footer className={styles.foote}>
+    <footer className={styles.footer}>
       <p>&copy; 2024 Simple Blog. All rights reserved.</p>
     </footer>
   );
